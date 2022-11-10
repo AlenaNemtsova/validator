0bc241e42abf0c216a6ea59dfe97fc06518db7eb
Alena Nemtsova <jelenatbs@gmail.com>
2022-11-10 23:49:29 +0300

    added regex

--- validator.js
+++ validator.js
@@ -11,14 +11,16 @@ class Validator {
 
     isDomain(input) {
         let isValidDomain = false;
-        if (input !== '')
+        const domainFormat = /([\w-]+\.)+\w+/g;
+        if (input.match(domainFormat))
             isValidDomain = true;
         return isValidDomain;
     }
 
     isDate(input) {
         let isValidDate = false;
-        if (input !== '')
+        const dateFormat = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+        if (input.match(dateFormat))
             isValidDate = true;
         return isValidDate;
 
@@ -54,14 +56,16 @@ class ValidatorStatic {
 
     static isDomain(input) {
         let isValidDomain = false;
-        if (input !== '')
+        const domainFormat = /([\w-]+\.)+\w+/g;
+        if (input.match(domainFormat))
             isValidDomain = true;
         return isValidDomain;
     }
 
     static isDate(input) {
         let isValidDate = false;
-        if (input !== '')
+        const dateFormat = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+        if (input.match(dateFormat))
             isValidDate = true;
         return isValidDate;
 
